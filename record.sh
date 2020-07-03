47176d127e0812e96b707df9da1f96039b410122
José Albornoz <jojo@eljojo.net>
2020-07-03 01:37:54 -0400

    try to stream directly into s3

--- record.sh
+++ record.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-mkdir -p /tmp/record/
-
 while true; do
   START_RECORD=$(date +%s)
-  ffmpeg -hide_banner -i "$RECORD_URL" -acodec copy /tmp/record/record.aac && aws s3 cp /tmp/record/record.aac "s3://$RECORD_BUCKET/$RECORD_FOLDER/$START_RECORD.aac" || sleep 1
+  ffmpeg -hide_banner -i "$RECORD_URL" -acodec copy - | aws s3 cp - "s3://$RECORD_BUCKET/$RECORD_FOLDER/$START_RECORD.aac" || sleep 1
 done
