fb4fbfe16fc42344018c00f421f4e50691f31de6
José Albornoz <jojo@eljojo.net>
2020-07-03 02:28:08 -0400

    revert streaming stuff, didn't work as well

--- record.sh
+++ record.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+mkdir -p /tmp/record/
+
 while true; do
   START_RECORD=$(date +%s)
-  ffmpeg -hide_banner -i "$RECORD_URL" -acodec copy -f adts -nostats - | buffer | aws s3 cp - "s3://$RECORD_BUCKET/$RECORD_FOLDER/$START_RECORD.aac"
+  ffmpeg  -hide_banner -nostats -loglevel error -i "$RECORD_URL" -acodec copy /tmp/record/record.aac && aws s3 cp /tmp/record/record.aac "s3://$RECORD_BUCKET/$RECORD_FOLDER/$START_RECORD.aac"
   sleep 1
 done
