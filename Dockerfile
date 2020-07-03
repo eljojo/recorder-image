2acd363f8f5ac1250fde924bc52314674414d92b
José Albornoz <jojo@eljojo.net>
2020-07-03 00:48:16 -0400

    set new stuff for record

--- Dockerfile
+++ Dockerfile
@@ -6,3 +6,6 @@ RUN apt-get update
 RUN apt-get install -y ca-certificates ffmpeg awscli && \
     rm -rf /var/lib/apt/lists/*
 
+RUN mkdir -p /app
+COPY record.sh /app/record.sh
+ENTRYPOINT ["/bin/bash", "/app/record.sh"]
---
+++ record.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+mkdir -p /tmp/record/
+
+while true; do
+  START_RECORD=$(date +%s)
+  ffmpeg -i "$RECORD_URL" -acodec copy /tmp/record/record.aac && aws s3 cp /tmp/record/record.aac "s3://$RECORD_BUCKET/$RECORD_FOLDER/$START_RECORD.aac"]
+done
