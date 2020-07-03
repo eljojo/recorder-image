864689ef3808d66031de86e22a9f62f6181ccbce
José Albornoz <jojo@eljojo.net>
2020-07-03 01:32:32 -0400

    minimize ffmpeg logging

--- record.sh
+++ record.sh
@@ -3,5 +3,5 @@ mkdir -p /tmp/record/
 
 while true; do
   START_RECORD=$(date +%s)
-  ffmpeg -i "$RECORD_URL" -acodec copy /tmp/record/record.aac && aws s3 cp /tmp/record/record.aac "s3://$RECORD_BUCKET/$RECORD_FOLDER/$START_RECORD.aac" || sleep 1
+  ffmpeg -hide_banner -i "$RECORD_URL" -acodec copy /tmp/record/record.aac && aws s3 cp /tmp/record/record.aac "s3://$RECORD_BUCKET/$RECORD_FOLDER/$START_RECORD.aac" || sleep 1
 done
