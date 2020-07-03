dbd7a746ad8449a4c596b0d25d779fa275529dd7
José Albornoz <jojo@eljojo.net>
2020-07-03 02:01:08 -0400

    try to fix bugs

--- record.sh
+++ record.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 while true; do
   START_RECORD=$(date +%s)
-  ffmpeg -hide_banner -i "$RECORD_URL" -acodec copy - | aws s3 cp - "s3://$RECORD_BUCKET/$RECORD_FOLDER/$START_RECORD.aac" || sleep 1
+  ffmpeg -hide_banner -i "$RECORD_URL" -acodec copy -f adts - | aws s3 cp - "s3://$RECORD_BUCKET/$RECORD_FOLDER/$START_RECORD.aac"
+  sleep 1
 done
