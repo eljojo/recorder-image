9a9298286a2cd057f86a4054688874332c90b838
José Albornoz <jojo@eljojo.net>
2020-07-03 02:31:53 -0400

    remove previous sessions before recording again

--- record.sh
+++ record.sh
@@ -2,6 +2,7 @@
 mkdir -p /tmp/record/
 
 while true; do
+  rm -rf /tmp/record/record.aac
   START_RECORD=$(date +%s)
   ffmpeg  -hide_banner -nostats -loglevel error -i "$RECORD_URL" -acodec copy /tmp/record/record.aac && aws s3 cp /tmp/record/record.aac "s3://$RECORD_BUCKET/$RECORD_FOLDER/$START_RECORD.aac"
   sleep 1
